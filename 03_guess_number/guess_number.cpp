ee4709dad7044c68cfe9d85c004ac9a63193218b
Anastasia Astakhova <nansy@sibmail.com>
2021-12-29 23:29:39 +0100

    added optional tasks 1 and 2

--- 03_guess_number/guess_number.cpp
+++ 03_guess_number/guess_number.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include "high_score.h"
 
-int main(){
+int main(int argc, char** argv){
+    int max_value = 100;
+    // parse command line arguments
+	if (argc >= 2) {
+		std::string arg1_value{ argv[1] };
+		if (arg1_value == "-max") {
+            if (argc < 3) {
+				std::cout << "Wrong usage! The argument '-max' requires some value!" << std::endl;
+				return -1;
+			}
+			// parse the string to the int value
+			max_value = std::stoi(argv[2]);
+			std::cout << "max value was set to " << max_value << std::endl;
+		}
+
+        if (arg1_value == "-table") {
+            read_high_score_file();
+            std::exit(0);
+        }
+	}
+
 	int current_value = 0;
 	bool not_win = true;
-    const int max_value = 100;
 
 	std::srand(std::time(nullptr)); // use current time as seed for random generator
 
-	const int random_value = std::rand() % 100;
-    const std::string high_scores_filename = "high_scores.txt";
+	const int random_value = std::rand() % max_value;
 
 	// Ask about name
 	std::cout << "Hi! Enter your name, please:" << std::endl;
@@ -36,49 +54,7 @@ int main(){
 
 	} while(true);
 
-    // Write new high score to the records table
-	{
-		// We should open the output file in the append mode - we don't want
-		// to erase previous results.
-		std::ofstream out_file {high_scores_filename, std::ios_base::app};
-		if (!out_file.is_open()) {
-			std::cout << "Failed to open file for write: " << high_scores_filename << "!" << std::endl;
-			return -1;
-		}
-
-		// Append new results to the table:
-		out_file << user_name << ' ';
-		out_file << attempts_count;
-		out_file << std::endl;
-	} // end of score here just to mark end of the logic block of code
-
-	// Read the high score file and print all results
-	{
-		std::ifstream in_file {high_scores_filename};
-		if (!in_file.is_open()) {
-			std::cout << "Failed to open file for read: " << high_scores_filename << "!" << std::endl;
-			return -1;
-		}
-
-		std::cout << "High scores table:" << std::endl;
-
-		std::string username;
-		int high_score = 0;
-		while (true) {
-			// Read the username first
-			in_file >> username;
-			// Read the high score next
-			in_file >> high_score;
-			// Ignore the end of line symbol
-			in_file.ignore();
-
-			if (in_file.fail()) {
-				break;
-			}
-
-			// Print the information to the screen
-			std::cout << username << '\t' << high_score << std::endl;
-		}
-	}
+    write_high_score(user_name, attempts_count);
+	read_high_score_file();
     return 0;
 }
---
+++ 03_guess_number/high_score.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void write_high_score(std::string user_name, int attempts_count);
+void read_high_score_file();
